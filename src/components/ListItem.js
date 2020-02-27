d06ad637922252092340ecf743c81de4a123cd91
Jaime Segura <jseguraweb@gmail.com>
2020-02-27 17:45:24 +0100

    improve postits

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -27,12 +27,14 @@ export default class ListItem extends Component {
         let opacityPostIt = this.state.check ? 0 : 1;
         let opacityPostIt2 = this.state.check ? 1 : 0;
         let changeTextDecoration = this.state.check === true ? 'line-through' : 'none';
+        let isDone = this.state.check === true ? 'DONE' : 'TO DO';
 
         return (
             <article>
                 <div className="post-it" style={{ opacity: opacityPostIt }}></div>
                 <div className="post-it-2" style={{ opacity: opacityPostIt2 }}></div>
                 <p className="note" style={{ textDecoration: changeTextDecoration }}>{this.props.text}</p>
+                <p className="is-done">{isDone}</p>
                 <button className="btn check" style={{ opacity: opacityCheck }} onClick={this.changeStatus}><FaCheck /></button>
                 <button className="btn cross" style={{ opacity: opacityCross }} onClick={this.changeStatus}><FaTimes /></button>
                 <button className="btn trash" onClick={() => this.deleteItem(this.props.text)}><FaTrash /></button>
