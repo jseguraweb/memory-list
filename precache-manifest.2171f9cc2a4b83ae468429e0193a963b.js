149c061b7478619e4cccbfb7e753bf23bdb1908b
Jaime Segura <jseguraweb@gmail.com>
2020-02-27 17:53:49 +0100

    Updates

---
+++ precache-manifest.2171f9cc2a4b83ae468429e0193a963b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbac68c19d2af0988f6885c19b0a8a19",
+    "url": "/memory-list/index.html"
+  },
+  {
+    "revision": "6556ca4b2189db795dbb",
+    "url": "/memory-list/static/css/main.7c4738ba.chunk.css"
+  },
+  {
+    "revision": "ac54719f106fceca1c26",
+    "url": "/memory-list/static/js/2.9cd1776a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/memory-list/static/js/2.9cd1776a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6556ca4b2189db795dbb",
+    "url": "/memory-list/static/js/main.081a1674.chunk.js"
+  },
+  {
+    "revision": "964c2dcb49f982ad89f4",
+    "url": "/memory-list/static/js/runtime-main.f1c7f64e.js"
+  },
+  {
+    "revision": "d3c0c152af0fe6b15633c304988834a5",
+    "url": "/memory-list/static/media/board.d3c0c152.jpg"
+  },
+  {
+    "revision": "ab9fb559983bf16816a328cf9a5b5965",
+    "url": "/memory-list/static/media/postit.ab9fb559.png"
+  },
+  {
+    "revision": "802518470c9d46090ec6f083564126d1",
+    "url": "/memory-list/static/media/postit2.80251847.png"
+  }
+]);
