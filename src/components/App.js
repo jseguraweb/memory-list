89c989e78dd048befe6c1e44d46ea732a6942d7b
Jaime Segura <jseguraweb@gmail.com>
2020-03-03 14:22:34 +0100

    set localStorage to save items

--- src/components/App.js
+++ src/components/App.js
@@ -10,25 +10,35 @@ export default class App extends Component {
     items: []
   }
 
+  componentDidMount() {
+    let data = localStorage.getItem('todo-list');
+    let savedTasks = JSON.parse(data);
+    if (data) {
+      this.setState({
+        items: savedTasks
+      })
+    }
+  }
+
   getData = data => {
     let newState = [...this.state.items, data]
     this.setState({
       items: newState
-    })
+    }, () => localStorage.setItem('todo-list', JSON.stringify(this.state.items)))
   }
 
   editData = (oldTask, newData) => {
     let newState = [...this.state.items].map(item => item.task === oldTask ? item.task = newData : item);
     this.setState({
       items: newState
-    })
+    }, () => localStorage.setItem('todo-list', JSON.stringify(this.state.items)))
   }
 
   removeItem = item => {
     let newState = this.state.items.filter(el => el.task !== item);
     this.setState({
       items: newState
-    })
+    }, () => localStorage.setItem('todo-list', JSON.stringify(this.state.items)))
   }
 
   render() {
